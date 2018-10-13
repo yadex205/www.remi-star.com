4fac3f22da728c3649c905d9dd42649e9269b691
Kanon Kakuno <yadex205@outlook.jp>
2018-10-13 14:11:19 +0900

    :fire: Remove redux, unused components.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,1 @@
-import { wrapWithProvider } from './src/lib/store';
 import './src/assets/css/app.sass';
-
-export const wrapRootElement = wrapWithProvider;
--- src/components/templates/general/_general.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
-
-export default ({ children }: Props) => (
-  <div className="t-general">{children}</div>
-);
