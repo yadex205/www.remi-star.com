67471787bd23eb62532ed9ef38add426d7334bf0
Kanon Kakuno <yadex205@outlook.jp>
2018-10-13 20:45:01 +0900

    :+1: Add home-section

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="o-footer">
       <div className="o-footer__inner">
         <div className="o-footer__copy">
-          &copy; 2017-present re-mi All rights resetved.
+          &copy; 2017-present re-mi All rights reserved.
         </div>
         <Link className="o-footer__policy-link" to="/policy">site policy</Link>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import General from 'components/templates/general';
 import MainVisual from 'components/molecules/main-visual';
+import HomeSection, { HomeSectionColumn } from 'components/molecules/home-section';
 
 export default function Index() {
   return (
     <General>
+      <Helmet>
+        <body className="p-home" />
+      </Helmet>
+
       <MainVisual />
+
+      <HomeSection>
+        <HomeSectionColumn title="live schedule">
+
+        </HomeSectionColumn>
+        <HomeSectionColumn title="twitter">
+
+        </HomeSectionColumn>
+      </HomeSection>
+
+      <HomeSection>
+        <HomeSectionColumn title="news">
+
+        </HomeSectionColumn>
+      </HomeSection>
     </General>
   );
 }
