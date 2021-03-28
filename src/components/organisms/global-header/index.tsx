9dcba0393fd9531117b66200a0bc5bc8b5416c1b
Kanon Kakuno <yadex205@outlook.jp>
2021-03-28 15:10:55 +0900

    :+1: improve logo

--- src/components/organisms/global-header/index.tsx
+++ src/components/organisms/global-header/index.tsx
@@ -6,7 +6,7 @@ import logoWebSiteImage from 'images/logo-website.svg';
 export const GlobalHeader: React.FC = () => (
   <header className="o-global-header">
     <Link className="o-global-header__brand-wrapper" to="/">
-      <img className="o-global-header__brand" alt="れーみ" src={logoWebSiteImage} />
+      <img className="o-global-header__brand" alt="れーみ" src={logoWebSiteImage} width="96" height="32" />
     </Link>
   </header>
 );
