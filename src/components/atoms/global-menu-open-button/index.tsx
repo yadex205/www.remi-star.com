9c94882986598291e1d7b9c4c04630f79cf1b678
Kanon Kakuno <yadex205@outlook.jp>
2021-03-28 15:13:20 +0900

    :+1: improve a-global-menu-open-button

--- src/components/atoms/global-menu-open-button/index.tsx
+++ src/components/atoms/global-menu-open-button/index.tsx
@@ -5,7 +5,7 @@ import { Icon } from 'components/atoms/icon';
 type GlobalMenuOpenButtonProps = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className' | 'children'>;
 
 export const GlobalMenuOpenButton: React.FC<GlobalMenuOpenButtonProps> = props => (
-  <button {...props} className="a-global-menu-open-button">
+  <button {...props} className="a-global-menu-open-button" aria-label="メニューを開く">
     <Icon name="list" />
   </button>
 );
